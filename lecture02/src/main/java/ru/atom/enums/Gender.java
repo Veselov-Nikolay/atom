a334a2880472b85cf03d608a891004020dcf77a1
rybalkinsd <yan.brikl@gmail.com>
2017-02-28 21:40:21 +0300

    lecture02 presentation

--- lecture02/src/main/java/ru/atom/enums/Gender.java
+++ lecture02/src/main/java/ru/atom/enums/Gender.java
@@ -8,6 +8,13 @@ public enum Gender {
     Female,
     Other;
 
+    /**
+     * Check couple.
+     *
+     * @param country - target country
+     * @param partner - available partner
+     * @return legal status
+     */
     public boolean isLegalCouple(Country country, Gender partner) {
         switch (country) {
             case Russia:
